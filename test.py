3cd70647e03e4779ebcf8da0410c198cbdd8935c
nico <nico.marcq@gmail.com>
2016-08-03 21:28:58 +0200

    add buzzer test

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import bv4242 as b
 import i2c
 
 # main
-
+import RPi.GPIO as gpio
 
 if __name__ == '__main__':
     # # prepare a queue to share data betwen threads
@@ -18,13 +18,27 @@ if __name__ == '__main__':
     # keypad_thread = KeypadManager(shared_queue)
     # keypad_thread.start()
 
-    arduino = ArduinoManager()
-
-    arduino.start_siren()
+    # arduino = ArduinoManager()
+    #
+    # arduino.start_siren()
     import time
-    time.sleep(3)
-    arduino.stop_siren()
-    time.sleep(3)
-    arduino.delayed_siren()
+    # time.sleep(3)
+    # arduino.stop_siren()
+    # time.sleep(3)
+    # arduino.delayed_siren()
+
+    BUZZER_PIN = 4
+    gpio.cleanup()
+    gpio.setmode(gpio.BCM)
+    gpio.setup(BUZZER_PIN, gpio.OUT)  # Relay enable
+    # defini le port GPIO 4 comme etant une sortie output
+    gpio.output(BUZZER_PIN, gpio.HIGH)
+    time.sleep(2)
+
+    gpio.output(BUZZER_PIN, gpio.LOW)
+
+    time.sleep(2)
+
+    gpio.output(BUZZER_PIN, gpio.HIGH)
 
 
